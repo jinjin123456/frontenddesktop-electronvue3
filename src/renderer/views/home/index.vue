af0c25561a97093bc5d268400d13e85c0e0b5644
wrjin <wrjin@iflytek.com>
2023-12-17 18:51:29 +0800

    feat:暂存

---
+++ src/renderer/views/home/index.vue
@@ -0,0 +1,12 @@
+<script lang="tsx">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    return () => <div>1111111111</div>
+  }
+})
+</script>
+
+<style lang="scss"></style>
